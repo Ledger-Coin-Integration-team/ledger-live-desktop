9c208c02f8c64bba3c8510eef34639bbf24dce46
Rémi Jarasson (Ext) <remi.jarasson-ext@ledger.fr>
2021-03-23 11:16:24 +0100

    feat(Polkadot): warn before staking if balance too low to earn rewards - COIN-1410

--- src/renderer/families/polkadot/EarnRewardsInfoModal.js
+++ src/renderer/families/polkadot/EarnRewardsInfoModal.js
@@ -1,16 +1,20 @@
 // @flow
-import React, { useCallback } from "react";
+import React, { useCallback, useMemo } from "react";
 import { Trans, useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 
 import type { Account, AccountLike } from "@ledgerhq/live-common/lib/types";
+import { getAccountUnit, getMainAccount } from "@ledgerhq/live-common/lib/account";
+import { formatCurrencyUnit } from "@ledgerhq/live-common/lib/currencies";
+import { usePolkadotPreloadData } from "@ledgerhq/live-common/lib/families/polkadot/react";
+import { hasEnoughAvailableBalanceForStaking } from "@ledgerhq/live-common/lib/families/polkadot/logic";
 
 import { urls } from "~/config/urls";
 import { openURL } from "~/renderer/linking";
 import { openModal, closeModal } from "~/renderer/actions/modals";
 import EarnRewardsInfoModal from "~/renderer/components/EarnRewardsInfoModal";
 import LinkWithExternalIcon from "~/renderer/components/LinkWithExternalIcon";
-import WarnBox from "~/renderer/components/WarnBox";
+import Alert from "~/renderer/components/Alert";
 
 type Props = {
   name?: string,
@@ -21,6 +25,10 @@ type Props = {
 export default function PolkadotEarnRewardsInfoModal({ name, account, parentAccount }: Props) {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const { minRewarded } = usePolkadotPreloadData();
+  const mainAccount = getMainAccount(account, parentAccount);
+  const unit = getAccountUnit(account);
+
   const onNext = useCallback(() => {
     dispatch(closeModal(name));
     dispatch(
@@ -29,6 +37,22 @@ export default function PolkadotEarnRewardsInfoModal({ name, account, parentAcco
       }),
     );
   }, [account, dispatch, name]);
+
+  const minRewardedFormatted = useMemo(
+    () =>
+      formatCurrencyUnit(unit, minRewarded, {
+        disableRounding: false,
+        alwaysShowSign: false,
+        showCode: true,
+        showAllDigits: false,
+      }),
+    [unit, minRewarded],
+  );
+
+  const showMinRewardedWarning = useMemo(
+    () => !hasEnoughAvailableBalanceForStaking(mainAccount, minRewarded),
+    [mainAccount, minRewarded],
+  );
 
   return (
     <EarnRewardsInfoModal
@@ -40,7 +64,19 @@ export default function PolkadotEarnRewardsInfoModal({ name, account, parentAcco
         t("polkadot.bond.steps.starter.bullet.1"),
         t("polkadot.bond.steps.starter.bullet.2"),
       ]}
-      additional={<WarnBox>{t("polkadot.bond.steps.starter.warning")}</WarnBox>}
+      additional={
+        <>
+          {showMinRewardedWarning && (
+            <Alert type="warning">
+              <Trans
+                i18nKey="polkadot.bond.steps.starter.minRewarded"
+                values={{ amount: minRewardedFormatted }}
+              />
+            </Alert>
+          )}
+          <Alert type="help">{t("polkadot.bond.steps.starter.warning")}</Alert>
+        </>
+      }
       footerLeft={
         <LinkWithExternalIcon
           label={<Trans i18nKey="polkadot.bond.steps.starter.help" />}
