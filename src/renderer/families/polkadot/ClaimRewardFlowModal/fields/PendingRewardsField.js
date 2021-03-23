d3bc00abd416142d05017ef110f383f70fccb837
haammar-ledger <hakim.aammar-ext@ledger.fr>
2021-03-23 18:12:37 +0100

    UI improvements after design review

--- src/renderer/families/polkadot/ClaimRewardFlowModal/fields/PendingRewardsField.js
+++ src/renderer/families/polkadot/ClaimRewardFlowModal/fields/PendingRewardsField.js
@@ -9,29 +9,31 @@ import { urls } from "~/config/urls";
 import { getDefaultExplorerView, getAddressExplorer } from "@ledgerhq/live-common/lib/explorers";
 import { getAccountUnit } from "@ledgerhq/live-common/lib/account";
 import type { Account } from "@ledgerhq/live-common/lib/types";
-import type { Transaction, PolkadotPendingReward } from "@ledgerhq/live-common/lib/families/polkadot/types";
+import type {
+  Transaction,
+  PolkadotPendingReward,
+} from "@ledgerhq/live-common/lib/families/polkadot/types";
 import { usePendingRewardsIdentities } from "@ledgerhq/live-common/lib/families/polkadot/react";
 
 import type { ThemedComponent } from "~/renderer/styles/StyleProvider";
 import { openURL } from "~/renderer/linking";
 import Box from "~/renderer/components/Box";
 import ScrollLoadingList from "~/renderer/components/ScrollLoadingList";
-import { Rotating } from "~/renderer/components/Spinner";
+import Spinner from "~/renderer/components/Spinner";
 import LinkWithExternalIcon from "~/renderer/components/LinkWithExternalIcon";
 import Text from "~/renderer/components/Text";
 import InfoCircle from "~/renderer/icons/InfoCircle";
-import IconLoader from "~/renderer/icons/Loader";
-import { colors } from "~/renderer/styles/theme";
 
 import PendingRewardRow from "./PendingRewardRow";
 
 const PendingRewardsInfo: ThemedComponent<{}> = styled(Box).attrs(p => ({
   horizontal: true,
   alignItems: "center",
+  borderRadius: 4,
   py: "8px",
   px: 3,
-  bg:"palette.divider",
-  color:"palette.text.shade100",
+  bg: "palette.divider",
+  color: "palette.text.shade100",
   fontSize: 4,
   ff: "Inter|SemiBold",
 }))`
@@ -47,6 +49,7 @@ const Placeholder: ThemedComponent<*> = styled(Box).attrs(() => ({
   mb: 2,
   p: 3,
   flex: "1",
+  fontSize: 15,
 }))`
   border: 1px solid ${p => p.theme.colors.palette.divider};
   margin-right: ${p => p.theme.overflow.trackSize}px;
@@ -81,7 +84,7 @@ const PendingRewardsField = ({
   const unit = getAccountUnit(account);
 
   const onLearnMore = useCallback(() => openURL(urls.stakingPolkadot), []);
-  
+
   const explorerView = getDefaultExplorerView(account.currency);
 
   const onExternalLink = useCallback(
@@ -111,58 +114,56 @@ const PendingRewardsField = ({
 
   return (
     <>
-    {/* TODO: rework this info box with new alert system */}
-    <PendingRewardsInfo
-      flex="1"
-      borderRadius={4}
-    >
-      <Box mr={2}>
-        <InfoCircle size={12} />
-      </Box>
-      <Box horizontal flex="1" style={{ wordBreak: "break-all" }}>
-        <Text
-          ff="Inter|SemiBold"
-          textAlign="left"
-          pr={1}
-          fontSize={3}
-          style={{ wordBreak: "break-word" }}
-        >
-          <Trans i18nKey="polkadot.claimReward.steps.selectReward.info" />
-        </Text>
-        <LinkWithExternalIcon
-          label={<Trans i18nKey="common.learnMore" />}
-          onClick={onLearnMore}
-        />
-      </Box>
-    </PendingRewardsInfo>
-    <Box id="pending-rewards-list">
+      {/* TODO: rework this info box with new alert system */}
+      <PendingRewardsInfo flex="1">
+        <Box mr={2}>
+          <InfoCircle size={12} />
+        </Box>
+        <Box horizontal justifyContent="space-between" flex="1" style={{ wordBreak: "break-all" }}>
+          <Text ff="Inter|SemiBold" textAlign="left" style={{ wordBreak: "break-word" }}>
+            <Trans i18nKey="polkadot.claimReward.steps.selectReward.info" />
+          </Text>
+          <LinkWithExternalIcon
+            label={<Trans i18nKey="common.learnMore" />}
+            onClick={onLearnMore}
+          />
+        </Box>
+      </PendingRewardsInfo>
+      <Box id="pending-rewards-list">
         <ScrollLoadingList
           data={pendingRewardsWithIdentities}
           style={{ flex: "1 0 240px" }}
           renderItem={renderItem}
-          noResultPlaceholder={<NoResultPlaceholder rewardsLoading={rewardsLoading}/>}
+          noResultPlaceholder={<NoResultPlaceholder rewardsLoading={rewardsLoading} />}
         />
       </Box>
     </>
   );
 };
 
-export const NoResultPlaceholder = ({
-  rewardsLoading,
-}: {
-  rewardsLoading: boolean
-}) => (
+export const NoResultPlaceholder = ({ rewardsLoading }: { rewardsLoading: boolean }) => (
   <Placeholder>
-    {rewardsLoading ?
-      <Box horizontal>
-        <Rotating size={16} mr={1}>
-          <IconLoader color={colors.grey} size={16} />
-        </Rotating>
-        <Trans i18nKey="polkadot.claimReward.steps.selectReward.loading" />
+    {rewardsLoading ? (
+      <Box vertical textAlign="center" alignItems="center">
+        <Spinner size={30} mb={4} />
+        <Text>
+          <Trans i18nKey="polkadot.claimReward.steps.selectReward.loading.info" />
+        </Text>
+        <Text>
+          <Trans i18nKey="polkadot.claimReward.steps.selectReward.loading.description" />
+        </Text>
       </Box>
-    :
-      <Trans i18nKey="polkadot.claimReward.steps.selectReward.noResults" />
-    }
+    ) : (
+      <Box vertical textAlign="center" alignItems="center">
+        <Box mb={4}>
+          <InfoCircle size={30} />
+        </Box>
+        <Text>
+          <Trans i18nKey="polkadot.claimReward.steps.selectReward.noResults" />
+        </Text>
+        <></>
+      </Box>
+    )}
   </Placeholder>
 );
 
